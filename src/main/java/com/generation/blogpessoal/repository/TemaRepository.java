d8bb5faf00106e001ccc8b2fd092c4efc9491853
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2023-12-12 09:23:06 -0300

    Implementação do método para buscar por descrição.

--- src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Tema;
 
 public interface TemaRepository extends  JpaRepository<Tema, Long>{
+
+	// Select * from tb_postagem Where tb_postagem.titulo like %descricao%;
+	
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 
 }
