5eac80e39ad0785003e4721f0c9c113570ed1496
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2023-12-12 08:51:33 -0300

    Criação da model Tema

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,42 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@NotBlank(message = "O atributo descricao é obrigatório!")
+	@Size(min = 5, max = 255, message = "O atributo descricao deve conter no mínimo 5 e no máximo 255 caracteres.")
+	@Column(name="descricao", length=255, nullable = false)
+	public String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
